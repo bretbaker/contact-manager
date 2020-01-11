0e1da08b17eb359baf739fbd6dae984e80c39d2b
Bret Baker <bretbaker808@gmail.com>
2020-01-11 13:41:24 -0800

    frontend start

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,31 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: []
+  };
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
