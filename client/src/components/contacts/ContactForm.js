368838fa3324fd81f820b18f11886e3e53ba64c8
Bret Baker <bretbaker808@gmail.com>
2020-01-16 12:53:22 -0800

    need to fix contact filter

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,112 @@
+import React, { useState, useContext, useEffect } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+
+const ContactForm = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { addContact, current, clearCurrent, updateContact } = contactContext;
+
+  useEffect(() => {
+    if (current !== null) {
+      setContact(current);
+    } else {
+      setContact({
+        name: '',
+        email: '',
+        phone: '',
+        type: ''
+      });
+    }
+  }, [contactContext, current]);
+
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    type: ''
+  });
+
+  const { name, email, phone, type } = contact;
+
+  const onChange = e =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (current === null) {
+      addContact(contact);
+    } else {
+      updateContact(contact);
+    }
+    clearAll();
+  };
+
+  const clearAll = () => {
+    clearCurrent();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h2 className='text-primary'>
+        {current ? 'Edit Contact' : 'Add Contact'}
+      </h2>
+      <input
+        type='text'
+        placeholder='Name'
+        name='name'
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        type='email'
+        placeholder='Email'
+        name='email'
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type='text'
+        placeholder='Phone'
+        name='phone'
+        value={phone}
+        onChange={onChange}
+      />
+      <h5 className='m'>Contact Type</h5>
+      <input
+        type='radio'
+        name='type'
+        value='personal'
+        check={type && 'personal'}
+        onChange={onChange}
+        className='m'
+      />{' '}
+      Personal
+      <br />
+      <input
+        type='radio'
+        name='type'
+        value='professional'
+        check={type && 'professional'}
+        onChange={onChange}
+        className='m'
+      />{' '}
+      Professional
+      <div>
+        <input
+          type='submit'
+          value={current ? 'Update Contact' : 'Add Contact'}
+          className='btn btn-primary btn-block'
+        />
+      </div>
+      {current && (
+        <div>
+          <button className='btn btn-light btn-block' onClick={clearAll}>
+            Clear
+          </button>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default ContactForm;
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -14,13 +14,62 @@ import {
 
 const ContactState = props => {
   const initialState = {
-    contacts: []
+    contacts: [
+      {
+        id: 1,
+        name: 'Tom Jones',
+        email: 'tomjones@email.com',
+        phone: '111-111-1111',
+        type: 'professional'
+      }
+    ],
+    current: null,
+    filtered: null
   };
   const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
+
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
+
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
+
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
+
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
+
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
+
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
