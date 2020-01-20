f6d5d7cfe976250ce7127b3e2463450050d0dfdb
Bret Baker <bretbaker808@gmail.com>
2020-01-20 10:53:19 -0800

    resolve issue on server file

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the Contact Manager API...' })
-);
-
 // define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
